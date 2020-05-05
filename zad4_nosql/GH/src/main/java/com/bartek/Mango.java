760ca1eae2b2a8e3e1295cd6e6fd32e90f175765
bbbrtk <sobkowiak.bart@gmail.com>
2020-05-05 11:36:55 +0200

    add getters to db

--- zad4_nosql/GH/src/main/java/com/bartek/Mango.java
+++ zad4_nosql/GH/src/main/java/com/bartek/Mango.java
@@ -1,8 +1,17 @@
 package com.bartek;
 
+import com.bartek.models.Course;
+import com.bartek.models.Grade;
+import com.bartek.models.Squence;
+import com.bartek.models.Student;
 import com.mongodb.MongoClient;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+
+import static com.bartek.Storage.*;
 
 public class Mango {
     private final Datastore datastore;
@@ -16,7 +25,7 @@ public class Mango {
         this.datastore = morphia.createDatastore(client, "DATABASE");
         this.datastore.ensureIndexes();
         this.datastore.enableDocumentValidation();
-//        this.fillDummy(this);
+        this.generateFirstMango(this);
     }
 
     public static Mango getMangoIns() {
@@ -26,4 +35,83 @@ public class Mango {
     public static void setMangoIns(Mango mangoIns) {
         Mango.mangoIns = mangoIns;
     }
+
+    public void generateFirstMango(Mango mango) {
+        if (this.datastore.createQuery(Student.class).count() > 0
+                && this.datastore.createQuery(Course.class).count() > 0
+                && this.datastore.createQuery(Squence.class).count() > 0) {
+            System.out.println("DB not empty");
+        } else {
+
+            Course course1 = generateCourse("QWER");
+            Grade grade1 = generateGrade(course1);
+
+            Course course2 = generateCourse("ASDF");
+            Grade grade2 = generateGrade(course2);
+
+            Course course3 = generateCourse("ZXCV");
+            Grade grade3 = generateGrade(course3);
+
+            Student student1 = generateStudent("Jerry", "Arry");
+            Student student2 = generateStudent("Merry", "Berry");
+            Student student3 = generateStudent("Terry", "Cerry");
+
+            student1.setGrade(grade1);
+            student2.setGrade(grade2);
+            student3.setGrade(grade3);
+
+            this.datastore.save(new Squence(123456L, 4L, 4L));
+
+            this.datastore.save(course1);
+            this.datastore.save(course2);
+            this.datastore.save(course3);
+
+            this.datastore.save(grade1);
+            this.datastore.save(student1);
+
+            this.datastore.save(grade2);
+            this.datastore.save(student2);
+
+            this.datastore.save(grade3);
+            this.datastore.save(student3);
+        }
+    }
+
+    private synchronized Long nextStudentId() {
+        Squence id = datastore.findAndModify(
+                datastore.createQuery(Squence.class),
+                datastore.createUpdateOperations(Squence.class)
+                        .inc("studentIndex", 1));
+        return id.getStudentIndex();
+    }
+
+    private synchronized Long nextCourseId() {
+        Squence id = datastore.findAndModify(
+                datastore.createQuery(Squence.class),
+                datastore.createUpdateOperations(Squence.class)
+                        .inc("courseID", 1));
+        return id.getCourseID();
+    }
+
+    private synchronized Long nextGradeId() {
+        Squence id = datastore.findAndModify(
+                datastore.createQuery(Squence.class),
+                datastore.createUpdateOperations(Squence.class)
+                        .inc("gradeID", 1));
+        return id.getGradeID();
+    }
+
+    public Course getCourse(Long id) throws NotFoundException {
+        Course course = (Course) datastore.find(Course.class).field("id").equal(id);
+        if (course == null) throw  new NotFoundException();
+        return course;
+    }
+
+    public Student getStudent(Long id) throws NotFoundException{
+        Student student = (Student) datastore.find(Student.class).field("index").equal(id);
+        if (student == null) throw new NotFoundException();
+        return student;
+    }
+
+
 }
